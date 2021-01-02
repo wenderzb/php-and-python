9ec31525e451ec5883ab0d8cdbc30d6c0a852c77
wender zilo <wyndy10@hotmail.com>
2021-01-02 00:00:50 -0300

    New 1052.py

---
+++ 1052.py
@@ -0,0 +1,6 @@
+def main():
+    i = int(input())
+    m = ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    print(m[i])     
+
+main()
